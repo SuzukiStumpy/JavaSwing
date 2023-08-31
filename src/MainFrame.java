2ebf4ae80b251818da976cd052498a0c77b153d5
Mark Edwards <github@medwards-online.co.uk>
2023-08-31 17:50:17 +0100

    Adding action listener to control

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
@@ -13,6 +15,13 @@ public class MainFrame extends JFrame {
 
         textArea = new JTextArea();
         btn = new JButton("Click me!");
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Hello\n");
+            }
+        });
 
         add(textArea, BorderLayout.CENTER);
         add(btn, BorderLayout.SOUTH);
