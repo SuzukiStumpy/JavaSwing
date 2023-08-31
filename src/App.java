d56fc374932cf42e26192d9f4601f9c038db9c8e
Mark Edwards <github@medwards-online.co.uk>
2023-08-31 17:43:45 +0100

    Adding layout and basic controls

--- src/App.java
+++ src/App.java
@@ -5,10 +5,14 @@ public class App {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new JFrame("Hello World!");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(600, 500);
-                frame.setVisible(true);
+                /* Basic frame application
+                    JFrame frame = new JFrame("Hello World!");
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setSize(600, 500);
+                    frame.setVisible(true);
+                 */
+
+                new MainFrame();
             }
         });
     }
---
+++ src/MainFrame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    private JTextArea textArea;
+    private JButton btn;
+
+    public MainFrame() {
+        super("Hello World!");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Click me!");
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 500);
+        setVisible(true);
+    }
+}
